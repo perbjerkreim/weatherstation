9e214c02aff6e11499cf209edb65c23cf5f23cfd
perbjerkreim <pbjerkreim@gmail.com>
2022-08-27 21:18:54 +0200

    trenge en melding

---
+++ rain.py
@@ -0,0 +1,18 @@
+from itertools import count
+from gpiozero import Button
+
+rain_sensor = Button(6)
+BUCKET_SIZE = 0.2794
+count = 0
+
+def bucket_tipped():
+    global count
+    count += 1
+    print(count * BUCKET_SIZE)
+
+
+def reset_rainfall():
+    global count
+    count = 0
+
+rain_sensor.when_pressed = bucket_tipped
